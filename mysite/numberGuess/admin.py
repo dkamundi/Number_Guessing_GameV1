d7e1beb0a8e40910920dbd4bacb114da7f1bb4e8
WaruiAlfred <alfredwarui17@gmail.com>
2023-09-14 01:45:03 +0300

    create models

--- mysite/numberGuess/admin.py
+++ mysite/numberGuess/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GameSession
+
+admin.site.register(GameSession)
--- mysite/numberGuess/models.py
+++ mysite/numberGuess/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class GameSession(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    secret_number = models.IntegerField(null=True)
+    attempts = models.IntegerField(default=10)
+    guesses = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s game record"
