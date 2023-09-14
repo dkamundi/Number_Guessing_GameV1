3f9f8c79407103592e71acb579b617fab751ec0a
dkamundi <davekamundi@gmail.com>
2023-09-13 22:34:37 -0400

    Administrative tasks

--- mysite/manage.py
+++ mysite/manage.py
@@ -6,15 +6,21 @@ import sys
 
 def main():
     """Run administrative tasks."""
+
+    # Set the default Django settings module as 'mysite.settings'.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
+        # Import the execute_from_command_line function from the Django core management module.
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # Handle ImportError if Django is not installed.
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # Execute the specified Django management command from the command line arguments.
     execute_from_command_line(sys.argv)
 
 
