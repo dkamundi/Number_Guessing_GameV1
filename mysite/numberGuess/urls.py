93e688d3ab086e467e088cf925c1b24df3e10d4d
dkamundi <davekamundi@gmail.com>
2023-09-13 22:26:03 -0400

    URLs

--- mysite/numberGuess/urls.py
+++ mysite/numberGuess/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import register, login_view, play_game
 
+# Import views from the application's views module
+
 urlpatterns = [
     path('', register, name='register'),
     path('login/', login_view, name='login'),
