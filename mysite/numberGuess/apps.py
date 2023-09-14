dfa45c5b916b632d75b9aae4a630f7d9f9db364d
dkamundi <davekamundi@gmail.com>
2023-09-13 22:31:34 -0400

    Defining a configuration class for the app

--- mysite/numberGuess/apps.py
+++ mysite/numberGuess/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# Define a configuration class for the 'numberGuess' app.
 class NumberguessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'numberGuess'
--- mysite/numberGuess/forms.py
+++ mysite/numberGuess/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+# Define a custom registration form that extends UserCreationForm.
 class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
 
+# Define a custom login form that extends AuthenticationForm.
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
