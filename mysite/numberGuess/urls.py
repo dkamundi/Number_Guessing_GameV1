8952573fdad691e83f16beac7ec12f748cb45315
dkamundi <davekamundi@gmail.com>
2023-09-13 22:37:08 -0400

    Import views from the application's views module

--- mysite/numberGuess/urls.py
+++ mysite/numberGuess/urls.py
@@ -4,7 +4,12 @@ from .views import register, login_view, play_game
 # Import views from the application's views module
 
 urlpatterns = [
+    # Define a URL pattern for the registration page, mapped to the 'register' view.
     path('', register, name='register'),
+    
+    # Define a URL pattern for the login page, mapped to the 'login_view' view.
     path('login/', login_view, name='login'),
+
+    # Define a URL pattern for the game-playing page, mapped to the 'play_game' view.
     path('play/', play_game, name='play_game'),
 ]
